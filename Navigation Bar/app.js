492c1698982246e555bebd0565ae237e52a627bc
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2022-12-22 20:27:29 -0500

    Add files via upload

---
+++ Navigation Bar/app.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+  links.classList.toggle('show-links');
+  console.log(navToggle);
+  console.log(links);
+});
+
+
+// const navToggle = document.querySelector('.nav-toggle')
+// const links = document.querySelector('.links')
+
+// navToggle.addEventListener('click', function () {
+//   links.classList.toggle('show-links');
+// });
