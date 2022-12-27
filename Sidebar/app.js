90befb4764b234134c330bb7e83a0e34e705e9e0
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2022-12-27 16:32:10 -0500

    Update app.js

--- Sidebar/app.js
+++ Sidebar/app.js
@@ -1,17 +1,11 @@
 const toggleBtn = document.querySelector('.sidebar-toggle');
-const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.close-btn');
 
 toggleBtn.addEventListener('click', function () {
-  // console.log(sidebar.classList);
-  // if(sidebar.classList.contains('show-sidebar')) {
-  //   sidebar.classList.remove('show-sidebar')
-  // } else {
-  //   sidebar.classList.add('show-sidebar');
-  // }
-  sidebar.classList.toggle('show-sidebar');
+  sidebar.classList.toggle('show-sidebar')
 });
 
 closeBtn.addEventListener('click', function () {
-  sidebar.classList.remove('show-sidebar');
+  sidebar.classList.toggle('show-sidebar')
 });
