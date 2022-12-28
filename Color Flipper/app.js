d623c06cc20ba8767bb12167c2c7fc9d4184d2a9
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2022-12-27 20:33:39 -0500

    Update app.js

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -1,16 +1,15 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
-const btn = document.getElementById("btn");
-const clr = document.querySelector(".color");
-
-btn.addEventListener("click", function () {
-  // get random number between 0 - 3
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber);
-
-  document.body.style.backgroundColor = colors[randomNumber];
-  clr.textContent = colors[randomNumber];
-});
+const colors = ["green", "purple", "red", "blue", "rgba(164, 180, 21)"];
+const btn = document.getElementById('btn');
+const clr = document.querySelector('.color');
 
 function getRandomNumber() {
-  return Math.floor (Math.random() * colors.length);
-};
+  return Math.floor (Math.random() * colors.length)
+}
+
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber()
+    document.body.style.backgroundColor = colors[randomNumber]
+    clr.textContent = colors[randomNumber]
+    console.log(randomNumber)
+    console.log(clr)
+});
