7f172f4b944343b90220d2eb44c9b24f0307ebbd
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2023-07-24 20:25:02 -0500

    Update hex.js

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -3,18 +3,18 @@ const arrayBtn = document.getElementById('btn');
 const arrayColor = document.querySelector('.color');
 
 function randomnessIsAround() {
-    return Math.floor (Math.random() * array.length);
+    return Math.floor (Math.random() * array.length)
 };
 
 arrayBtn.addEventListener('click', function () {
     let hexColor = '#'
     for (let c = 0 ; c < 6 ; c++) {
-        const random = randomnessIsAround();
-        hexColor += array[random];
-        document.body.style.backgroundColor = hexColor;
-        arrayColor.textContent = hexColor;
-        console.log(hexColor);
-        console.log(random);
-        console.log(array);
+        const random = randomnessIsAround()
+        hexColor += array[random]
+        document.body.style.backgroundColor = hexColor
+        arrayColor.textContent = hexColor
+        console.log(hexColor)
+        console.log(random)
+        console.log(array)
     };
 });
