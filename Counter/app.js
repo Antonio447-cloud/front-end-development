cc1e835a3a203096542b9ecfb4388af06df0b277
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2023-07-25 21:23:54 -0500

    Update app.js

--- Counter/app.js
+++ Counter/app.js
@@ -1,26 +1,26 @@
 const btn = document.querySelectorAll('.btn');
-const btnValue = document.querySelector('#value');
+const btnColor = document.querySelector('#value');
 
-let counter = 0;
+let counter = 0
 
-btn.forEach(function(steps) {
-    steps.addEventListener('click', function(e) {
-        const random = e.currentTarget.classList
-        if (random.contains('decrease')) {
+btn.forEach(function (step) {
+    step.addEventListener('click', function(e) {
+        const tool = e.currentTarget.classList
+        if (tool.contains('decrease')) {
             counter--
-        } else if (random.contains('increase')) {
+        } else if (tool.contains('increase')) {
             counter++
-        } else (
+        } else {
             counter = 0
-        )
-        
+        } 
+
         if (counter < 0) {
-            btnValue.style.color = 'red'
+            btnColor.style.color = 'red'
         } else if (counter > 0) {
-            btnValue.style.color = 'green'
-        } else if (counter === 0) {
-            btnValue.style.color = '#222'
+            btnColor.style.color = 'green'
+        } else {
+            btnColor.style.color = '#222'
         }
-            btnValue.textContent = counter;
+            btnColor.textContent = counter
     });
 });
